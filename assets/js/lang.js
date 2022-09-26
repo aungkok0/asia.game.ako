8ebcd2766db989298a46feb53ac991afdecce381
aungkok0 <114368837+aungkok0@users.noreply.github.com>
2022-09-26 04:07:34 -0700

    Create lang.js

---
+++ assets/js/lang.js
@@ -0,0 +1,979 @@
+var dict = {
+    "Withdraw": {
+        cn: "取款",
+        en: "Withdraw",
+        my: "Pengeluaran"
+    },
+    "Total Balance": {
+        cn: "总额",
+        en: "Total Balance",
+        my: "Total Balance",
+        bm: "စုစုပေါင်းလက်ကျန်"
+    },
+
+    "Others": {
+        cn: "其他",
+        en: "Others",
+        my: "Lain-lain",
+        bm: "အခြားသူများ"
+    },
+
+    "Deposit": {
+        cn: "存款",
+        en: "Deposit",
+        my: "Deposit"
+    },
+    "Menu": {
+        cn: "菜单",
+        en: "Menu",
+        my: "Menu",
+        bm: "မီနူး။"
+    },
+    "Type": {
+        cn: "种类",
+        en: "Type",
+        my: "Jenis",
+        bm: "အမျိုးအစား"
+    },
+    "Credit": {
+        cn: "总计",
+        en: "Credit",
+        my: "Kredit",
+        bm: "စုစုပေါင်း"
+    },
+
+    "Target": {
+        cn: "目标",
+        en: "Target",
+        my: "Kredit"
+    },
+    "Datetime": {
+        cn: "時間",
+        en: "Datetime",
+        my: "Tarikh",
+        bm: "အချိန်/ရက်စွဲ။"
+    },
+    "Current Amount": {
+        cn: "今日数额",
+        en: "Current Amount",
+        my: "Current Amount"
+    },
+    "Amount": {
+        cn: "数额",
+        en: "Amount",
+        my: "Amount",
+        bm: "ပမာန။"
+    },
+    "Remark": {
+        cn: "标记",
+        en: "Remark",
+        my: "Teguran",
+        bm: "မှတ်ချက်"
+    },
+    "Status": {
+        cn: "状态",
+        en: "Status",
+        my: "Status",
+        bm: "အခေြအနေ"
+    },
+
+    "Settle Amount": {
+        cn: "昨日数额",
+        en: "Settle Amount",
+        my: "Settle Amount"
+    },
+
+    "TURNOVER": {
+        cn: "总流水",
+        en: "TURNOVER",
+        my: "TURNOVER"
+    },
+
+    "History": {
+        cn: "交易记录",
+        en: "History",
+        my: "Transaksi",
+        bm: "မှတ်တမ်း"
+    },
+    "Language": {
+        cn: "语言",
+        en: "Language",
+        my: "Bahasa",
+        bm: "ဘာသာစကား"
+    },
+    "Setting": {
+        cn: "设置",
+        en: "Setting",
+        my: "Setting"
+    },
+    "Log out": {
+        cn: "登出",
+        en: "Logout",
+        my: "Logout",
+        bm: "အကောက်ထွက်သည်"
+
+
+    },
+    "Login": {
+        cn: "登入",
+        en: "Login",
+        my: "Login",
+        bm: "လော့ဂ်အင်"
+    },
+    "Contact Us": {
+        cn: "联系我们",
+        en: "Contact Us",
+        my: "Hubungi Kami"
+    },
+    "Register": {
+        cn: "注册",
+        en: "Register",
+        my: "Daftar"
+    },
+
+    "Home": {
+        cn: "主页",
+        en: "Home",
+        my: "Home"
+    },
+    "Profile": {
+        cn: "账户",
+        en: "Profile",
+        my: "Profil"
+    },
+    "Loading": {
+        cn: "正在加载",
+        en: "Loading",
+        my: "Loading"
+    },
+    "Sportsbook": {
+        cn: "体育投注",
+        en: "Sportsbook",
+        my: "Sportsbook"
+    },
+    "Processing": {
+        cn: "处理当中",
+        en: "Processing",
+        my: "Processing"
+    },
+    "Live Casino": {
+        cn: "真人娱乐",
+        en: "Live Casino",
+        my: "Live Casino"
+    },
+    "Log in": {
+        cn: "登入",
+        en: "Login",
+        my: "Login"
+
+    },
+    "Sign In": {
+        cn: "登入",
+        en: "Sign In",
+        my: "Sign In",
+        bm: "ဝင်ပါ။",
+
+    },
+
+
+    "All Games": {
+        cn: "所有",
+        en: "All Games",
+        my: "Semua"
+    },
+
+    "All": {
+        cn: "所有",
+        en: "All",
+        my: "Semua",
+        bm: "အားလုံး"
+    },
+    "ALL": {
+        cn: "所有",
+        en: "ALL",
+        my: "Semua",
+        bm: "အားလုံး"
+    },
+
+    "Slot Games": {
+        cn: "电子游戏",
+        en: "Slot Games",
+        my: "Permainan slot"
+    },
+
+    "Live Casino": {
+        cn: "真人娱乐",
+        en: "Live Casino",
+        my: "Kasino"
+    },
+    "Arcade": {
+        cn: "街机",
+        en: "Arcade",
+        my: "Arcade"
+    },
+
+    "Sport n Race": {
+        cn: "体育投注",
+        en: "Sport & Race",
+        my: "Sportsbook"
+    },
+    "4D n Lottery": {
+        cn: "彩票投注",
+        en: "4D & Lottery",
+        my: "4D & Loteri"
+    },
+    "Bonus": {
+
+        cn: "优惠活动",
+
+        en: "Bonus",
+
+        my: "Bonus"
+
+    },
+    "Promotion": {
+
+        cn: "优惠活动",
+
+        en: "Promotion",
+
+        my: "Promosi"
+
+    },
+    "Banking": {
+
+        cn: "转账教学",
+
+        en: "Banking",
+
+        my: "Banking"
+
+    },
+    "Play Now": {
+        cn: "立即开始",
+        en: "Play Now",
+        my: "Play Now"
+    },
+
+    "Password": {
+        cn: "更改密码",
+        en: "Password",
+        my: "Password",
+        bm: "စကားဝှက်"
+    },
+    "Current Password": {
+        cn: "目前密码",
+        en: "Current Password",
+        my: "Current Password",
+        bm: "လက်ရှိစကားဝှက်"
+    },
+    "Change Password": {
+        cn: "更改密码",
+        en: "Change Password",
+        my: "Change Password",
+        bm: "အသုံးပြုသူအမည်"
+    },
+    "Forget Password": {
+        cn: "忘记密码",
+        en: "Forget Password",
+        my: "Forget Password"
+    },
+
+    "Forget Username": {
+        cn: "忘记账号",
+        en: "Forget Username",
+        my: "Forget Username"
+    },
+    "Username": {
+        cn: "账号",
+        en: "Username",
+        my: "Username",
+        bm: "အသုံးပြုသူအမည်"
+    },
+
+    "Reset Pin-Code": {
+        cn: "重置安全码",
+        en: "Reset Pin-Code",
+        my: "Reset Pin-Code"
+    },
+
+    "Play": {
+        cn: "进入",
+        en: "Play",
+        my: "Play"
+    },
+    "Coming Soon": {
+        cn: "即将来临",
+        en: "Coming Soon",
+        my: "Akan Datang"
+    },
+    "Install": {
+        cn: "下载",
+        en: "Install",
+        my: "Install"
+    },
+
+    "Transfer-Out": {
+        cn: "转出",
+        en: "Transfer-Out",
+        my: "Pengeluaran"
+    },
+    "Transfer-In": {
+        cn: "转进",
+        en: "Transfer-In",
+        my: "Pengeluaran"
+    },
+    "Submit": {
+        cn: "提交",
+        en: "Submit",
+        my: "Submit",
+        bm: "တင်ပြပါ။"
+    },
+
+
+    "Upload bank slip": {
+        cn: "上传汇款单据",
+        en: "Upload bank slip",
+        my: "Upload bank slip"
+    },
+    "SELECT BANK": {
+        cn: "选择银行",
+        en: "SELECT BANK",
+        my: "PILIH BANK"
+    },
+    "Method": {
+        cn: "选择方法",
+        en: "Method",
+        my: "Cara"
+    },
+    "Deposit Amount": {
+        cn: "存款数额",
+        en: "Deposit Amount",
+        my: "Deposit Amoun"
+    },
+    "Maint.": {
+        cn: "维护",
+        en: "Maint.",
+        my: "Maint."
+    },
+    "No Bonus": {
+        cn: "不要索取任何优惠",
+        en: "No Bonus",
+        my: "Tiada Bonus"
+    },
+    "Deposit Date": {
+        cn: "汇款日期",
+        en: "Deposit Date",
+        my: "Tarikh Deposit"
+    },
+
+    "Deposit Time": {
+        cn: "汇款时间",
+        en: "Deposit Date",
+        my: "Masa Deposit"
+    },
+    "No Bonus": {
+        cn: "不要索取任何优惠",
+        en: "No Bonus",
+        my: "Tiada Bonus"
+    },
+    "* Please upload bank slip for quicker process": {
+        cn: "* 请上汇款单据 以加速处理时间",
+        en: "* Please upload bank slip for quicker process.",
+        my: "Silah upload bank slip untuk process lebih cepat."
+    },
+
+    "* Please fill in the deposit amount": {
+        cn: "* 请填写充值金额",
+        en: "* Please fill in the deposit amount.",
+        my: "Silah upload bank slip untuk process lebih cepat."
+    },
+
+    "* Please choose a transfer method": {
+        cn: "* 请选择转账方法",
+        en: "* Please choose a transfer method.",
+        my: "Silah upload bank slip untuk process lebih cepat."
+    },
+
+    "Play Now": {
+        cn: "进入",
+        en: "Play Now",
+        my: "Play Now"
+    },
+
+    "Withdraw": {
+        cn: "提取",
+        en: "Withdraw",
+        my: "Pengeluaran"
+    },
+    "PLAY": {
+        cn: "进入",
+        en: "PLAY",
+        my: "PLAY"
+    },
+
+
+    "Amount": {
+        cn: "数额",
+        en: "Amount",
+        my: "Amoun",
+        bm: "ပမာန။"
+    },
+
+
+    "Bank": {
+        cn: "银行转账进分",
+        en: "Bank",
+        my: "Bank"
+    },
+
+    "Payment Gateway": {
+        cn: "快速进分",
+        en: "Payment Gateway",
+        my: "Payment Gateway"
+    },
+
+    "Crypto Currencies": {
+        cn: "USDT",
+        en: "Crypto Currencies",
+        my: "Crypto Currencies"
+    },
+
+    "Account Number": {
+        cn: "银行户口",
+        en: "Account Number",
+        my: "Nombor Akuan"
+    },
+
+    "Datetime": {
+        cn: "时间日期",
+        en: "Datetime",
+        my: "Takrikh",
+        bm: "အချိန်/ရက်စွဲ။"
+    },
+
+    "Status": {
+        cn: "状态",
+        en: "Status",
+        my: "Status",
+        bm: "အခေြအနေ"
+    },
+
+    "Pending": {
+        cn: "等待处理",
+        en: "Processing",
+        my: "pemprosesan",
+    },
+
+    "Rejected": {
+        cn: "不通过",
+        en: "Rejected",
+        my: "Ditolak",
+    },
+
+    "Rebate": {
+        cn: "回水",
+        en: "Rebate",
+        my: "Ditolak"
+    },
+
+    "Cash": {
+        cn: "现金",
+        en: "Cash",
+        my: "Ditolak"
+    },
+
+    "Cash-In": {
+        cn: "转入",
+        en: "Cash-In",
+        my: "Ditolak"
+    },
+
+    "Cash-Out": {
+        cn: "转出",
+        en: "Cash-Out",
+        my: "Ditolak"
+    },
+
+    "agent": {
+        cn: "代理",
+        en: "agent",
+        my: "Ditolak"
+    },
+
+    "Revice": {
+        cn: "收款",
+        en: "Revice",
+        my: "Ditolak"
+    },
+
+    "Credit": {
+        cn: "分数",
+        en: "Credit",
+        my: "Ditolak",
+        bm: "စုစုပေါင်း"
+    },
+
+    "From": {
+        cn: "来自",
+        en: "From",
+        my: "Ditolak"
+    },
+
+    "Complete": {
+        cn: "通过",
+        en: "Complete",
+        my: "Diluluskan"
+    },
+
+    "Claimed": {
+        cn: "已领取",
+        en: "Claimed",
+        my: "Diluluskan"
+    },
+
+    "Unclaimed": {
+        cn: "未领取",
+        en: "Unclaimed",
+        my: "Diluluskan"
+    },
+
+    "Approved": {
+        cn: "通过",
+        en: "Approved",
+        my: "Diluluskan"
+    },
+
+    "Previous request under process": {
+        cn: "已有请求正在处理",
+        en: "Previous request under process",
+        my: "Permintaan terdahulu masih dalam proses"
+    },
+
+    "Game": {
+        cn: "游戏",
+        en: "Game",
+        my: "Game",
+        bm: "ဂိမ်း။"
+    },
+
+    "Summary Credit": {
+        cn: "总计数额",
+        en: "Credit Summary",
+        my: "Ringkasan Kredit",
+        bm: "အကြွေးအကျဉ်းချုပ်"
+    },
+    "Main Wallet": {
+        cn: "主钱包",
+        en: "Main Wallet",
+        my: "Dompet Utama",
+        bm: "အဓိကပိုက်ဆံအိတ်။"
+    },
+    "TOTAL": {
+        cn: "总额",
+        en: "TOTAL",
+        my: "JUMLAH",
+        bm: "စုစုပေါင်း။"
+    },
+    "Total": {
+        cn: "总共",
+        en: "Total",
+        my: "Jumlah",
+        bm: "စုစုပေါင်း။"
+    },
+    "My Profile": {
+        cn: "我的账户",
+        en: "My Profile",
+        my: "Profil",
+        bm: "ငါ့အကောင့်"
+    },
+
+    "User ID": {
+        cn: "登入身份",
+        en: "User ID",
+        my: "User ID"
+    },
+
+    "Tel": {
+        cn: "手机号码",
+        en: "Mobile Number",
+        my: "Nombor Tel"
+    },
+
+    "Email": {
+        cn: "电邮地址",
+        en: "Email",
+        my: "Email"
+    },
+
+    "Bank Account Number": {
+        cn: "银行户口",
+        en: "Bank Account Number",
+        my: "Nombor Bank Akaun"
+    },
+
+    "If any changes please contact customer service": {
+        cn: "若有更改请联系客服",
+        en: "If any changes please contact customer service",
+        my: "Kalau ada perubahan sila hubungi khidmat Pelanggan"
+    },
+
+    "FAQs": {
+        cn: "常见问题",
+        en: "FAQs",
+        my: "FAQs"
+    },
+    "How to Join": {
+        cn: "如何加入",
+        en: "How to join",
+        my: "How to join"
+    },
+    "How to Top-up": {
+        cn: "如何充值",
+        en: "How to Top up",
+        my: "How to Top up",
+    },
+
+    "How to Withdraw": {
+        cn: "如何取现",
+        en: "How to Withdraw",
+        my: "How to Withdraw",
+    },
+    "How to Transfer": {
+        cn: "如何转移资金",
+        en: "How to Transfer",
+        my: "How to Transfer",
+    },
+
+
+
+
+
+
+
+
+
+    "HOME": {
+        cn: "主页",
+        en: "HOME",
+        my: "HOME",
+    },
+    "WITHDRAW": {
+        cn: "取款",
+        en: "WITHDRAW",
+        my: "Pengeluaran"
+    },
+
+    "TRANSFER": {
+        cn: "转移",
+        en: "TRANSFER",
+        my: "Pemindahan"
+    },
+
+    "Transfer": {
+        cn: "转移",
+        en: "Transfer",
+        my: "Pemindahan"
+    },
+
+    "DEPOSIT": {
+        cn: "存款",
+        en: "DEPOSIT",
+        my: "DEPOSIT"
+    },
+    "CREDIT": {
+        cn: "总计",
+        en: "CREDIT",
+        my: "KREDIT",
+        bm: "စုစုပေါင်း"
+    },
+
+    "HISTORY": {
+        cn: "交易记录",
+        en: "HISTORY",
+        my: "TRANSAKSI"
+    },
+    "LOGOUT": {
+        cn: "登出",
+        en: "LOGOUT",
+        my: "LOGOUT"
+    },
+    "PROFILE": {
+        cn: "账户",
+        en: "PROFILE",
+        my: "PROFIL"
+    },
+    "PROMOTION": {
+        cn: "优惠活动",
+        en: "PROMOTION",
+        my: "PROMOTIO"
+    },
+    "PROMO": {
+        cn: "优惠活动",
+        en: "PROMO",
+        my: "PROMOTION"
+    },
+    "中文": {
+        cn: "ENG",
+        en: "中文",
+        my: "ENG"
+    },
+    "LIVE CHAT": {
+        cn: "客服",
+        en: "LIVE CHAT",
+        my: "ENG"
+    },
+    "CONTACT": {
+        cn: "联系我们",
+        en: "CONTACT",
+        my: "ENG"
+    },
+    "SLOTS": {
+        cn: "电子游戏",
+        en: "SLOTS",
+        my: "SLOTS",
+        bm: "အားလုံးဂိမ်းများ"
+    },
+    "Slot": {
+        cn: "电子游戏",
+        en: "Slot",
+        my: "Slot",
+        bm: "အားလုံးဂိမ်းများ"
+    },
+    "Slot": {
+        cn: "电子游戏",
+        en: "Slot",
+        my: "Slot"
+    },
+    "CASINO": {
+        cn: "真人娱乐",
+        en: "CASINO",
+        my: "CASINO",
+        bm: "တိုက်ရိုက်ကာစီနိ"
+    },
+    "Casino": {
+        cn: "真人娱乐",
+        en: "CASINO",
+        my: "CASINO",
+        bm: "တိုက်ရိုက်ကာစီနိ"
+    },
+    "LOTTERY": {
+        cn: "彩票",
+        en: "LOTTERY",
+        my: "ENG"
+    },
+    "Lottery": {
+        cn: "彩票",
+        en: "Lottery",
+        my: "ENG"
+    },
+    "SPORTS": {
+        cn: "体育投注",
+        en: "SPORTS",
+        my: "ENG"
+    },
+    "FISH": {
+        cn: "捕鱼",
+        en: "FISH",
+        my: "FISH",
+        bm: "ငါးဖမ်း"
+    },
+    "4D": {
+        cn: "4D",
+        en: "4D",
+        my: "ENG"
+    },
+    "CHAT": {
+        cn: "客服",
+        en: "CHAT",
+        my: "ENG"
+    },
+    "DOWNLOAD": {
+        cn: "下载",
+        en: "DOWNLOAD",
+        my: "ENG"
+    },
+    "REBATE": {
+        cn: "回水",
+        en: "REBATE",
+        my: "ENG"
+    },
+    "LOGIN / JOIN": {
+        cn: "登入 / 注册",
+        en: "LOGIN / JOIN",
+        my: "ENG"
+    },
+    "Welcome !": {
+        cn: "欢迎 ！",
+        en: "Welcome !",
+        my: "ENG"
+    },
+    "Log in for details": {
+        cn: "登入获取更多详情",
+        en: "Log in for details",
+        my: "ENG"
+    },
+    "WHAT'S NEW": {
+        cn: "最新消息",
+        en: "WHAT'S NEW",
+        my: "ENG"
+    },
+    "LATEST PROMOTION": {
+        cn: "最新优惠",
+        en: "LATEST PROMOTION",
+        my: "ENG"
+    },
+    "Back to Home page": {
+        cn: "回到主页",
+        en: "Back to Home page",
+        my: "ENG"
+    },
+    "Phone": {
+        cn: "电话号码",
+        en: "Phone",
+        my: "ENG"
+    },
+
+
+
+
+
+
+
+
+
+    "LOGIN": {
+        cn: "登入",
+        en: "LOGIN",
+        my: "LOGIN",
+        bm: "လော့ဂ်အင်"
+    },
+    "SIGN UP": {
+        cn: "注册",
+        en: "SIGN UP",
+        my: "ENG"
+    },
+    "LIVE CASINO": {
+        cn: "真人娱乐",
+        en: "LIVE CASINO",
+        my: "ENG"
+    },
+    "RECOMMENDED GAMES": {
+        cn: "游戏推荐",
+        en: "RECOMMENDED GAMES",
+        my: "ENG"
+    },
+    "SLOT GAMES": {
+        cn: "电子游戏",
+        en: "SLOT GAMES",
+        my: "ENG"
+    },
+    "PLAY NOW": {
+        cn: "游玩",
+        en: "PLAY NOW",
+        my: "ENG"
+    },
+    "FEATURED VIDEOS": {
+        cn: "精选视频",
+        en: "FEATURED VIDEOS",
+        my: "ENG"
+    },
+    "SPORT": {
+        cn: "体育投注",
+        en: "SPORT",
+        my: "SPORT",
+        bm: "အားကစား။"
+    },
+    "BANKING": {
+        cn: "转账教学",
+        en: "BANKING",
+        my: "ENG"
+    },
+    "Slots Game": {
+        cn: "电子游戏",
+        en: "Slots Game",
+        my: "ENG"
+    },
+    "Sport": {
+        cn: "体育投注",
+        en: "Sport",
+        my: "ENG"
+    },
+    "QRCODE": {
+        cn: "二维码",
+        en: "QRCODE",
+        my: "ENG"
+    },
+
+
+
+    "Phone Number": {
+        cn: "手机号码",
+        en: "Phone Number",
+        my: "ENG"
+    },
+    "New Password": {
+        cn: "新密码",
+        en: "New Password",
+        my: "Kata Laluan Baharu",
+        bm: "စကားဝှက်အသစ်"
+    },
+    "Re-Enter New Password": {
+        cn: "确认新密码",
+        en: "Re-Enter New Password",
+        my: "Re-Enter New Password",
+        bm: "စကားဝှက်အသစ်ကို ပြန်ထည့်ပါ။"
+    },
+    "Confirm New Password": {
+        cn: "确认新密码",
+        en: "Confirm New Password",
+        my: "ENG",
+        bm: "စကားဝှက်အသစ်ကို အတည်ပြုပါ။"
+    },
+    "Verification Code": {
+        cn: "验证码",
+        en: "Verification Code",
+        my: "ENG"
+    },
+    "Game Credit": {
+        cn: "游戏分数",
+        en: "Game Credit",
+        my: "Permainan Kredit",
+        bm: "ဂိမ်းခရက်ဒစ်"
+    },
+    "Transfer Out": {
+        cn: "转出",
+        en: "Transfer Out",
+        my: "Pengeluaran",
+        bm: "လွှဲပေးလိုက်ပါ။"
+    },
+    "Transfer In": {
+        cn: "转进",
+        en: "Transfer In",
+        my: "Pengeluaran",
+        bm: "လွှဲပြောင်း"
+    },
+    "Launch": {
+        cn: "开始游戏",
+        en: "Launch",
+        my: "Mula Permainan",
+        bm: "ယခုကစားပါ"
+    },
+
+}
+
+if (localStorage.getItem("lang") === null || (localStorage.getItem("lang") != "en" && localStorage.getItem("lang") != "cn" && localStorage.getItem("lang") != "bm")) {
+    console.log('Hard setting lang');
+    localStorage.setItem("lang", 'en');
+} else {
+    console.log('lang loaded');
+    console.log(localStorage.getItem("lang"));
+}
+var translator = $('html').translate({
+    lang: "en",
+    t: dict
+});
+
+$(document).ready(function() {
+    translator.lang(localStorage.getItem("lang"));
+});
